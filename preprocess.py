a2de449f5fb6d334a91b894b5850c2cbfb9f83a6
Joseph Nicolls <jnicolls@stanford.edu>
2017-11-01 23:32:23 -0700

    update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -1,5 +1,8 @@
 import pandas
 import requests, json
+from pybedtools import BedTool
+
+SENTINEL = -1
 
 regionsFile = "input/table1_list_vista_enchancer_elements.csv"
 sequencingDatasetsFile = "input/table2_sequencingDatasets.csv"
@@ -9,17 +12,33 @@ HEADERS = {'accept': 'application/json'}
 def getEncodeURL(urlSnippet):
 	return encodeParentURL + urlSnippet + "/?frame=object"
 
-def getJSONResponse(urlSnippet):
+def getEncodeJSONResponse(urlSnippet):
 	response = requests.get(getEncodeURL(urlSnippet), headers=HEADERS)
 	return response.json()
 
 def processEncodeDataset(accession):
-	responseExperimentDict = getJSONResponse(accession)
+	responseExperimentDict = getEncodeJSONResponse(accession)	
 	dataset = responseExperimentDict["dataset"]
-	responseDatasetDict = getJSONResponse(dataset)
-	origFileDict = getJSONResponse(responseDatasetDict["original_files"][7])
+	responseDatasetDict = getEncodeJSONResponse(dataset)
 
-	print json.dumps(origFileDict, indent=4, separators=(',', ': '))
+	bedFilepath = SENTINEL
+
+	for encodeFile in responseDatasetDict["original_files"]:
+		encodeFileDict = getEncodeJSONResponse(encodeFile)
+		if "file_type" in encodeFileDict.keys() and "output_type" in encodeFileDict.keys():
+			if encodeFileDict["file_type"] == "bed narrowPeak" and encodeFileDict["output_type"] == "replicated peaks":
+				bedFilepath = encodeFileDict["href"]
+				break
+
+	if bedFilepath == SENTINEL:
+		raise ValueError("dataset " + dataset + " had no valid file for accession value " + accession)
+
+	bedFile = requests.get(getEncodeURL(bedFilepath))
+	bedFileContent = BedTool(bedFile)
+
+
+	print(len(bedFileContent))
+
 
 def processSequencingDatasets():
 	sequencingDatasets = pandas.read_csv(sequencingDatasetsFile)
