393b7cc077e434582325a3c51253d9351adb07c3
Joseph Nicolls <jnicolls@stanford.edu>
2017-11-01 23:33:38 -0700

    commented out unnecessary api calling code

--- preprocess.py
+++ preprocess.py
@@ -4,40 +4,40 @@ from pybedtools import BedTool
 
 SENTINEL = -1
 
-regionsFile = "input/table1_list_vista_enchancer_elements.csv"
-sequencingDatasetsFile = "input/table2_sequencingDatasets.csv"
-encodeParentURL = "https://www.encodeproject.org/"
-HEADERS = {'accept': 'application/json'}
+#regionsFile = "input/table1_list_vista_enchancer_elements.csv"
+#sequencingDatasetsFile = "input/table2_sequencingDatasets.csv"
+#encodeParentURL = "https://www.encodeproject.org/"
+#HEADERS = {'accept': 'application/json'}
 
-def getEncodeURL(urlSnippet):
-	return encodeParentURL + urlSnippet + "/?frame=object"
+#def getEncodeURL(urlSnippet):
+#	return encodeParentURL + urlSnippet + "/?frame=object"
 
-def getEncodeJSONResponse(urlSnippet):
-	response = requests.get(getEncodeURL(urlSnippet), headers=HEADERS)
-	return response.json()
+#def getEncodeJSONResponse(urlSnippet):
+#	response = requests.get(getEncodeURL(urlSnippet), headers=HEADERS)
+#	return response.json()
 
-def processEncodeDataset(accession):
-	responseExperimentDict = getEncodeJSONResponse(accession)	
-	dataset = responseExperimentDict["dataset"]
-	responseDatasetDict = getEncodeJSONResponse(dataset)
-
-	bedFilepath = SENTINEL
-
-	for encodeFile in responseDatasetDict["original_files"]:
-		encodeFileDict = getEncodeJSONResponse(encodeFile)
-		if "file_type" in encodeFileDict.keys() and "output_type" in encodeFileDict.keys():
-			if encodeFileDict["file_type"] == "bed narrowPeak" and encodeFileDict["output_type"] == "replicated peaks":
-				bedFilepath = encodeFileDict["href"]
-				break
-
-	if bedFilepath == SENTINEL:
-		raise ValueError("dataset " + dataset + " had no valid file for accession value " + accession)
-
-	bedFile = requests.get(getEncodeURL(bedFilepath))
-	bedFileContent = BedTool(bedFile)
-
-
-	print(len(bedFileContent))
+#def processEncodeDataset(accession):
+#	responseExperimentDict = getEncodeJSONResponse(accession)	
+#	dataset = responseExperimentDict["dataset"]
+#	responseDatasetDict = getEncodeJSONResponse(dataset)
+#
+#	bedFilepath = SENTINEL
+#
+#	for encodeFile in responseDatasetDict["original_files"]:
+#		encodeFileDict = getEncodeJSONResponse(encodeFile)
+#		if "file_type" in encodeFileDict.keys() and "output_type" in encodeFileDict.keys():
+#			if encodeFileDict["file_type"] == "bed narrowPeak" and encodeFileDict["output_type"] == "replicated peaks":
+#				bedFilepath = encodeFileDict["href"]
+#				break
+#
+#	if bedFilepath == SENTINEL:
+#		raise ValueError("dataset " + dataset + " had no valid file for accession value " + accession)
+#
+#	bedFile = requests.get(getEncodeURL(bedFilepath))
+#	bedFileContent = BedTool(bedFile)
+#
+#
+#	print(len(bedFileContent))
 
 
 def processSequencingDatasets():
